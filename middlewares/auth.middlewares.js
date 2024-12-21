a1b69edb924836343be0327d2d9657ad035f8949
ABDULLAHAZHERCH <abdullahazher11@gmail.com>
2024-12-22 00:18:51 +0500

    n

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -1,6 +1,59 @@
-import jwt from "jsonwebtoken";
+// import jwt from "jsonwebtoken";
 
-import User from '../models/usermodel.js'
+// import User from '../models/usermodel.js'
+// import AppError from "../utils/error.util.js";
+
+// /**
+//  * @isLoggedIn - Middleware to check if the user is authenticated.
+//  * Verifies the JWT token from the cookies and attaches the user details to the request object.
+//  * If no token is present or invalid, it returns an "Unauthenticated" error.
+//  */
+
+// const isLoggedIn = async (req, res, next)=>{
+//     const {token}= req.cookies;
+
+//     if(!token){
+//         return next(new AppError('Unauthenticated, pls login  again ', 401));
+//     }
+//     const userDetails= await jwt.verify(token , process.env.JWT_SECRET );
+
+//     req.user = userDetails;
+
+//     next();
+// }
+// /**
+//  * @authorizedRoles - Middleware to check if the user has authorized roles.
+//  * It ensures that the current user has one of the roles required to access the route.
+//  */
+// const authorizedRoles = (...roles)=>async(req, res, next)=>{
+//     const currentUserRoles =req.user.roles;
+//     if(roles.includes(currentUserRoles)){
+//         return next (
+//             new AppError("You do not have permission to acess this route", 400)
+//         )
+//     }
+//     next();
+// }
+// /**
+//  * @authorizedSubscriber - Middleware to check if the user has an active subscription.
+//  * If the user is not an admin and does not have an active subscription, it returns a "Forbidden" error.
+//  */
+// const authorizedSubscriber =async(req, res, next) =>{
+//     const user = await User.findById(id)
+//     const subscription = user.subscription.status
+//     const currentUserRole = user.role
+//     if (currentUserRole !== 'ADMIN' && subscription !== 'active') {
+//         return next(createError(403, "please subscribe to access this"))
+//     }
+// }
+// export{
+//     isLoggedIn,
+//     authorizedRoles,
+//     authorizedSubscriber,
+// }
+
+import jwt from "jsonwebtoken";
+import User from "../models/usermodel.js";
 import AppError from "../utils/error.util.js";
 
 /**
@@ -8,47 +61,58 @@ import AppError from "../utils/error.util.js";
  * Verifies the JWT token from the cookies and attaches the user details to the request object.
  * If no token is present or invalid, it returns an "Unauthenticated" error.
  */
+const isLoggedIn = async (req, res, next) => {
+  const { token } = req.cookies;
 
-const isLoggedIn = async (req, res, next)=>{
-    const {token}= req.cookies;
+  if (!token) {
+    return next(new AppError("Unauthenticated, please login again", 401));
+  }
 
-    if(!token){
-        return next(new AppError('Unauthenticated, pls login  again ', 401));
-    }
-    const userDetails= await jwt.verify(token , process.env.JWT_SECRET );
-
+  try {
+    const userDetails = jwt.verify(token, process.env.JWT_SECRET);
     req.user = userDetails;
-
     next();
-}
+  } catch (error) {
+    return next(
+      new AppError("Invalid or expired token, please login again", 401)
+    );
+  }
+};
+
 /**
  * @authorizedRoles - Middleware to check if the user has authorized roles.
  * It ensures that the current user has one of the roles required to access the route.
  */
-const authorizedRoles = (...roles)=>async(req, res, next)=>{
-    const currentUserRoles =req.user.roles;
-    if(roles.includes(currentUserRoles)){
-        return next (
-            new AppError("You do not have permission to acess this route", 400)
-        )
+const authorizedRoles =
+  (...roles) =>
+  async (req, res, next) => {
+    const currentUserRoles = req.user.roles;
+    if (!roles.includes(currentUserRoles)) {
+      return next(
+        new AppError("You do not have permission to access this route", 403)
+      );
     }
     next();
-}
+  };
+
 /**
  * @authorizedSubscriber - Middleware to check if the user has an active subscription.
  * If the user is not an admin and does not have an active subscription, it returns a "Forbidden" error.
  */
-const authorizedSubscriber =async(req, res, next) =>{
-    const user = await User.findById(id)
-    const subscription = user.subscription.status
-    const currentUserRole = user.role
-    if (currentUserRole !== 'ADMIN' && subscription !== 'active') {
-        return next(createError(403, "please subscribe to access this"))
-    }
-}
-export{
-    isLoggedIn,
-    authorizedRoles,
-    authorizedSubscriber,
-}
-    
+const authorizedSubscriber = async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    return next(new AppError("User not found", 404));
+  }
+
+  const subscription = user.subscription?.status || "inactive";
+  const currentUserRole = user.role;
+
+  if (currentUserRole !== "ADMIN" && subscription !== "active") {
+    return next(new AppError("Please subscribe to access this content", 403));
+  }
+
+  next();
+};
+
+export { isLoggedIn, authorizedRoles, authorizedSubscriber };
